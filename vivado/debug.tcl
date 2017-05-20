23b5a821dd961667ab00e29f9a37885fe8cb417f
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2017-05-20 15:47:01 +0900

    mod

--- vivado/debug.tcl
+++ vivado/debug.tcl
@@ -2,6 +2,7 @@
 
 set origin_dir .
 set proj_name zedboard
+set app_name hello
 
 set sdk_ws_dir $origin_dir/$proj_name/$proj_name.sdk
 
@@ -11,15 +12,15 @@ set top_module_name [lindex [split $hdf_filename_only .] 0]
 set hw_project_name ${top_module_name}_hw_platform_0
 
 connect
-
 targets -set -nocase -filter {name =~ "ARM*#0"}
 rst
+
 fpga $sdk_ws_dir/$hw_project_name/design_1_wrapper.bit
 loadhw $sdk_ws_dir/$hw_project_name/system.hdf
 source $sdk_ws_dir/$hw_project_name/ps7_init.tcl
 ps7_init
 ps7_post_config
-dow $sdk_ws_dir/hello/Debug/hello.elf
+dow $sdk_ws_dir/$app_name/Debug/$app_name.elf
 
 con
 
