9715497a58bf92262bd1cff0468c7d658303a375
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2017-05-24 13:05:50 +0900

    dma(stream) test

--- vivado/main.c
+++ vivado/main.c
@@ -13,22 +13,59 @@
 #define MEM_WIDTH (1 << MEMSIZE)
 #define BUF_WIDTH (1 << BUFSIZE)
 
-#define port(n) (XPAR_AXI_TOP_0_BASEADDR + 4*(n))
-static u32 *_port = (u32 *)0x43C10000;
-static u32 *mem = (u32 *)0x43C00000;
-static u32 mem_we    = 0;
-static u32 mem_addr  = 0;
-static u32 mem_wdata = 0;
-static u32 mem_rdata = 0;
+#define port(n)         (XPAR_AXI_TOP_0_BASEADDR + 4*(n))
+static u32 *_port     = (u32 *)0x43C10000;
+
+static u32 *mem       = (u32 *)0x43C00000;
+static u32  mem_we    = 0;
+static u32  mem_addr  = 0;
+static u32  mem_wdata = 0;
+static u32  mem_rdata = 0;
+
+#include "xaxidma.h"
+static XAxiDma dma;
+static XAxiDma_Config *dma_cfg;
+const int dma_id      = XPAR_AXIDMA_0_DEVICE_ID;
+
+#ifndef DDR_BASE_ADDR
+#warning CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H, \
+     DEFAULT SET TO 0x01000000
+#define MEM_BASE_ADDR   0x01000000
+#else
+#define MEM_BASE_ADDR   (DDR_BASE_ADDR + 0x1000000)
+#endif
+
+#define TX_BUFFER_BASE    (MEM_BASE_ADDR + 0x00100000)
+#define RX_BUFFER_BASE    (MEM_BASE_ADDR + 0x00300000)
+#define RX_BUFFER_HIGH    (MEM_BASE_ADDR + 0x004FFFFF)
+
+// latency analysis
+#include "xtime_l.h"
+static XTime begin, end;
+#define BEGIN XTime_GetTime(&begin);
+#define END   do { \
+                XTime_GetTime(&end); \
+                printf("%d %10.6f\n", \
+                       Index/4, (double)(end-begin) / COUNTS_PER_SECOND * 1000000); \
+              } while (0);
 
 #define assert_eq(a, b) do {                                  \
   if ((a) != (b)) {                                           \
     printf("Assertion failed: %s == %s, file %s, line %d\n",  \
-      #a, #b, __FILE__, __LINE__);                            \
+            #a, #b, __FILE__, __LINE__);                      \
     printf("\t%s == %lx, %s == %lx\n", #a, (a), #b, (b));     \
     printf("%s: FAIL\n", __func__);                           \
     return EXIT_FAILURE;                                      \
   }                                                           \
+} while (0)
+
+#define assert_not(cond, fail_msg) do {                 \
+  if ((cond)) {                                         \
+    printf("Assertion failed: %s, file %s, line %d\n",  \
+            (fail_msg), __FILE__, __LINE__);            \
+    printf("%s: FAIL\n", __func__);                     \
+    return EXIT_FAILURE;                                \
+  }                                                     \
 } while (0)
 
 #define test_return() do {          \
@@ -37,6 +74,37 @@ static u32 mem_rdata = 0;
 } while (0)
 
 typedef int test;
+
+void init_reg(void)
+{
+  int i;
+
+  for (i = 0; i < REG_WIDTH/2; i++)
+    Xil_Out32(port(i), 0x0);
+}
+
+void init_mem(void)
+{
+  memset(mem, 0, sizeof(u32)*MEM_WIDTH);
+}
+
+int init_buf(void)
+{
+  int status;
+
+  // Initialize the XAxiDma device.
+  dma_cfg = XAxiDma_LookupConfig(dma_id);
+  assert_not(!dma_cfg, "No config found");
+
+  status = XAxiDma_CfgInitialize(&dma, dma_cfg);
+  assert_not(status != XST_SUCCESS, "Initialization failed");
+
+  assert_not(XAxiDma_HasSg(&dma), "Device configured as SG mode");
+
+  // Disable interrupts, we use polling mode
+  XAxiDma_IntrDisable(&dma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
+  XAxiDma_IntrDisable(&dma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
+}
 
 void proc(int t)
 {
@@ -58,19 +126,6 @@ void proc(int t)
     default:
       break;
   }
-}
-
-void init_reg(void)
-{
-  int i;
-
-  for (i = 0; i < 32; i++)
-    Xil_Out32(port(i), 0x0);
-}
-
-void init_mem(void)
-{
-  memset(mem, 0, sizeof(u32)*MEM_WIDTH);
 }
 
 void print_reg(void)
@@ -203,6 +258,39 @@ test test_s_axi(void)
   test_return();
 }
 
+test test_s_axi_stream(void)
+{
+  int i;
+  int status;
+  u32 *src = (u32 *)TX_BUFFER_BASE;
+  u32 value;
+
+  // Flush the SrcBuffer before the DMA transfer, in case the Data Cache is enabled
+  Xil_DCacheFlushRange((UINTPTR)src, BUF_WIDTH);
+
+  value = 12;
+
+  for(i = 0; i < BUF_WIDTH; i++) {
+      src[i] = value;
+      value  = value + 1;
+  }
+
+  for(i = sizeof(u32); i < BUF_WIDTH; i += sizeof(u32)) {
+    status = XAxiDma_SimpleTransfer(&dma, (UINTPTR)src, i, XAXIDMA_DMA_TO_DEVICE);
+    assert_not(status != XST_SUCCESS, "Transfer failed");
+
+    BEGIN
+    while (XAxiDma_Busy(&dma, XAXIDMA_DMA_TO_DEVICE)) {
+      XTime_GetTime(&end);
+      assert_not((double)(end-begin) / COUNTS_PER_SECOND > 1.0,
+                  "More than 1sec was elapsed on dma");
+      /* Blocking */
+    }
+  }
+
+  test_return();
+}
+
 int main(void)
 {
   setbuf(stdout, NULL);
@@ -212,11 +300,13 @@ int main(void)
 
   init_reg();
   init_mem();
+  init_buf();
 
   // probe();
 
   test_s_axi_lite();
   test_s_axi();
+  test_s_axi_stream();
 
   puts("###  end  ##################################################");
 
